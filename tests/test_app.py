2e4168bb58cc461754f4a0823732ed68dd2e76e1
DaviAlencar2 <davilt.alencar@gmail.com>
2026-04-19 10:08:47 -0300

    test: cria primeiro teste

---
+++ tests/test_app.py
@@ -0,0 +1,24 @@
+from fastapi.testclient import TestClient
+
+from http import HTTPStatus
+
+from learning_fastapi.app import app
+
+
+def test_root_deve_retornar_ola():
+    """
+    Esse teste tem 3 etapas (AAA)
+    - A: Arrange
+    - A: Act
+    - A: Assert
+    """
+
+    #arrange
+    client = TestClient(app)
+
+    #act
+    response = client.get('/')
+
+    #assert
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'ola'}
